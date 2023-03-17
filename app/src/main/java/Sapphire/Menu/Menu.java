437f86c68356ae9c1d9d55fcd7f6fb9f4c5778bf
SomeStupidSongBird <563ftw@gmail.com>
2023-03-16 19:23:29 -0500

    importing and fixing imports from server

--- app/src/main/java/Sapphire/Menu/Menu.java
+++ app/src/main/java/Sapphire/Menu/Menu.java
@@ -3,9 +3,6 @@ package Sapphire.Menu;
 //#region imports
 import java.util.HashMap;
 import java.util.Scanner;
-import Sapphire.Tasks.ITaskManager;
-import Sapphire.Auth.IAuthorizor;
-import Sapphire.Logging.ILogger;
 import Sapphire.*;
 //#endregion imports
 
@@ -71,13 +68,7 @@ public class Menu implements Runnable{
             
             switch(getUserInput()){
                 case "1":
-                    manageRegisteredDevices();
-                    break;
-                case "2":
-                    viewActivityLog();
-                    break;
-                case "3":
-                    makeNewRequest();
+                    
                     break;
                 case "quit": // replace with return statement if resume execution is implemented
                 case "shutdown":
@@ -95,159 +86,6 @@ public class Menu implements Runnable{
         }
     }
     //#endregion MainMenu
-
-    //#region registerDevices
-    private void manageRegisteredDevices(){
-        Boolean printMenu = true;
-        while(true){
-            if(printMenu){
-                clearScreen();
-                System.out.println("Enter the number of your action:");
-                System.out.println("\t1. Register a New Device");
-                System.out.println("\t2. Remove a Device");
-                System.out.println("\t3. Show Registered Devices");
-                System.out.println("\t4. Return to Main Menu");
-                printMenu = false;
-            }
-            switch(getUserInput()){
-                case "1":
-                    // register new device
-                    registerDevice();
-                    return;
-                case "2":
-                    removeDevice();
-                    return;
-                case "3":
-                    showDevices();
-                    return;
-                case "4":
-                    return;
-                case "h":
-                case "H":
-                case "help":
-                case "Help":
-                    break;
-                default:
-                    continue;
-            }
-            printMenu = true;
-        }
-    }
-
-    private void registerDevice(){
-        String newDeviceName = null;
-        String newAuthToken = null;
-        boolean nameSelected = false;
-        boolean deviceAdded = false;
-        clearScreen();
-        do{
-            System.out.print("Enter the name of the new device: ");
-            System.out.flush();
-            newDeviceName = getUserInput();
-            System.out.print("Device name: "+newDeviceName+"\nIs this correct(Y/N)? ");
-            System.out.flush(); // just to make sure
-            String confirm = getUserInput();
-            if(cancel(confirm)) return;
-            switch(confirm.toLowerCase()){
-                case "yes":
-                case "y":
-                    nameSelected = true;
-                    break;
-                case "no":
-                case "n":
-                    continue;
-                default:
-                    System.out.print("Device name: "+newDeviceName+"\nIs this correct? ");
-                    System.out.flush(); // just to make sure
-            }
-        }while(!nameSelected);
-        clearScreen();
-        do{
-            System.out.print("Enter the authentication token of the new device:\n\t");
-            System.out.flush(); // just to make sure
-            newAuthToken = getUserInput();
-            if(cancel(newAuthToken)) return;
-            
-            if(newDeviceName==null||newAuthToken==null){
-                System.out.println("Something's going wrong: "+newDeviceName+","+newAuthToken);
-                waitForNextKeystroke();
-                return;
-            }
-
-            deviceAdded = authorizor.registerDevice(newDeviceName, newAuthToken);
-            //clearScreen();
-            if(deviceAdded){
-                System.out.println("Successfully added new device: "+newDeviceName);
-            }else{
-                System.out.println("Failed to register new device");
-            }
-            waitForNextKeystroke();
-            clearScreen();
-        }while(!deviceAdded);
-    }
-
-    private void removeDevice(){
-        String deviceName = "";
-        int deviceID = 0;
-
-        clearScreen();
-        while(true){
-            System.out.println("Enter the ID of the device you want to remove");
-            String sdeviceID = getUserInput();
-            if(cancel(sdeviceID)) return;
-            try{
-                deviceID=Integer.parseInt(sdeviceID);
-            }catch(Exception e){
-                System.out.println("Invalid input");
-                continue;
-            }
-            if(authorizor.checkDeviceExists(deviceID)){
-                try{deviceName = authorizor.getDeviceName(deviceID);}
-                catch(Exception e){
-                    System.err.println("Device name not found despite id being found");
-                }
-                System.out.println("Are you sure you want to remove: "+deviceName+"? (Y/N)");
-                String confirm = getUserInput();
-                if(cancel(confirm)) return;
-                switch(confirm.toLowerCase()){
-                    case "yes":
-                    case "y":
-                        break;
-                    case "no":
-                    case "n":
-                        return;
-                    default:
-                        System.out.println("Are you sure you want to remove: "+deviceName+"? (Y/N)");
-                }
-                clearScreen();
-                if(authorizor.removeDeviceAuth(deviceID)){
-                    System.out.println("Device: "+deviceID+"|"+deviceName+" Successfully removed\nPress Enter to return");
-                }else{
-                    System.out.println("Failed to remove device "+deviceID+"|"+deviceName+"\nPress Enter to return");    
-                }
-                waitForNextKeystroke();
-                return;
-            }
-        }
-    }
-
-    private void showDevices(){
-        clearScreen();
-        HashMap<Integer,String> devices = null;
-        System.out.println("All currently registered devices {Device_ID|Device_Name}\nPress Enter to return");
-        try{
-            devices = authorizor.showAllDevices();
-        }catch(Exception e){
-            System.err.println(e.getMessage());
-            waitForNextKeystroke();
-            return;    
-        }
-        for(int deviceid : devices.keySet()){
-            System.out.println("{"+deviceid+"|"+devices.get(deviceid)+"}");
-        }
-        waitForNextKeystroke();
-    }
-    //#endregion registerDevices
 
     //#region activityLog
     private void viewActivityLog(){
@@ -312,11 +150,6 @@ public class Menu implements Runnable{
         while(true){
             System.out.println("What device would you like to pull from?");
             // display all 
-            HashMap<Integer,String> devices = authorizor.showAllDevices();
-            if(devices==null) throw new Exception();
-            for(int deviceid : devices.keySet()){
-                System.out.print("{"+deviceid+"|"+devices.get(deviceid)+"}, ");
-            }
             String sdeviceID = getUserInput();
             if(cancel(sdeviceID)){
                 return;
@@ -362,11 +195,6 @@ public class Menu implements Runnable{
         while(true){
             System.out.println("What device would you like to send to?");
             // display all 
-            HashMap<Integer,String> devices = authorizor.showAllDevices();
-            if(devices==null) throw new Exception();
-            for(int deviceid : devices.keySet()){
-                System.out.print("{"+deviceid+"|"+devices.get(deviceid)+"}, ");
-            }
             String sdeviceID = getUserInput();
             if(cancel(sdeviceID)){
                 return;
---
+++ app/src/main/java/Sapphire/Networking/StructuredRequest.java
@@ -0,0 +1,94 @@
+package Sapphire.Networking;
+
+import java.util.regex.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class StructuredRequest {
+    //#region init
+    private static long fileID = 0;
+    public String authToken;
+    public int clientID; // -1 to start a new task
+    public int taskID;
+    public int targetID; // -1 if responding to the server
+    public HashMap<String,String> extraDetails;
+
+    public StructuredRequest(int clientID, int taskID, int targetID, String body){
+        /* constructor for the MenuMiniClient to make new requests */
+        
+        this.clientID = clientID;
+        this.taskID = taskID;
+        this.targetID = targetID;
+        extraDetails = new HashMap<String,String>();
+
+        for(String regionName : getRegionNames(body)){
+            // place the regions into the details to be indexed
+            // Since files are zipped and stored locally, there's no need to save the file's content locally with a new name
+            extraDetails.put(regionName, findRegionBody(body,regionName));
+        }
+    }
+
+    //#endregion init
+
+    //#region helpers
+    private String[] getRegionNames(String input){
+        String[] regionNames = new String[0];
+        // placing the body into a usable form based on the regions they're in
+        Pattern regionPattern = Pattern.compile("<(.)*>(.)*<\\/(.)*>");
+        Matcher matcher = regionPattern.matcher(input);
+        
+        // find a full region
+        while(matcher.find()){
+            String region = input.substring(matcher.start(),matcher.end()); // get a string of just that region
+            String regionName = region.substring(region.indexOf("<")+1,region.indexOf(">")-1); 
+            append(regionNames,regionName);
+        }
+        return regionNames;
+    }
+
+    private String[] append(String[] arr,String str){
+        String[] returnArr = new String[arr.length+1];
+        System.arraycopy(arr, 0, returnArr, 0, arr.length);
+        returnArr[returnArr.length-1] = str;
+        return returnArr;
+    }
+
+    private String findRegionBody(String input, String regionName){
+        Pattern regionPattern = Pattern.compile("<"+regionName+">.*<\\/"+regionName+">");
+        Matcher matcher = regionPattern.matcher(input);
+        // find a full region
+        if(matcher.find()){
+            String region = input.substring(matcher.start(),matcher.end()); // get a string of just that region
+            Pattern pat = Pattern.compile("<\\/*"+regionName+">"); 
+            Matcher mat = pat.matcher(region);
+            mat.find();
+            int start = mat.end();
+            mat.find();
+            int end = mat.start();
+            return region.substring(start,end);
+        }else{
+            return "error";
+        }
+    }
+
+    private int[] getFileBounds(byte[] input){
+        byte[] startRegionNameBytes = ("<File>").getBytes();
+        byte[] endRegionNameBytes = ("</File>").getBytes();
+        int regionNameSize = startRegionNameBytes.length;
+        int[] regionBounds = {-1,-1};
+        for(int i=0;i<input.length;i++){
+            byte[] slice = Arrays.copyOfRange(input, i, (i+regionNameSize));
+            byte[] slice2 = Arrays.copyOfRange(input, i, (i+regionNameSize+1));
+            if(Arrays.equals(slice, startRegionNameBytes)&&(regionBounds[0]==-1)){
+                regionBounds[0] = i+regionNameSize; 
+                i+= regionNameSize;
+            }else if(Arrays.equals(slice2, endRegionNameBytes)&&(regionBounds[1]==-1)){
+                regionBounds[1] = i-1;
+                break;
+            }
+        }
+        return regionBounds;
+    }
+    //#endregion helpers
+}
