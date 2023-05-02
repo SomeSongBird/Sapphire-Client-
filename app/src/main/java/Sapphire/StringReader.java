d6dd3515b57aabb0a1f82e18e24c0ec037da2dbd
SomeStupidSongBird <563ftw@gmail.com>
2023-05-02 12:09:01 -0500

    init startup changes

--- app/src/main/java/Sapphire/StringReader.java
+++ app/src/main/java/Sapphire/StringReader.java
@@ -4,12 +4,13 @@ import java.util.HashMap;
 import java.util.Scanner;
 import java.io.*;
 import java.nio.file.Files;
+import Sapphire.Menu.Menu;
 
 public class StringReader {
     static String inputfile;
     static HashMap<String,String> strings;
     public StringReader() throws FileNotFoundException{
-        inputfile = System.getProperty("user.dir")+"/resources/secret.input";      // change secret.input to strings.input
+        inputfile = System.getProperty("user.dir")+"/resources/strings.input";      // change secret.input to strings.input
         strings = new HashMap<String,String>();
         InputStream is;
         try {
@@ -24,16 +25,29 @@ public class StringReader {
             return;
         }
         Scanner inputReader = new Scanner(is);
+        Scanner userInput = new Scanner(System.in);
         while(inputReader.hasNext()){
             String nextLine = inputReader.nextLine();
             if(nextLine.length()>0){
-                if(nextLine.charAt(0)!='#'){ //basically a comment
-                    String[] seperatedLine = nextLine.split("::");
-                    if(seperatedLine.length==1){
-                        strings.put(seperatedLine[0],"");
-                    }else{
-                        strings.put(seperatedLine[0],seperatedLine[1]);
+                if(nextLine.charAt(0)=='#'){continue;}  //basically a comment
+                String[] seperatedLine = nextLine.split("::");
+                if(seperatedLine.length==1){
+                    if(seperatedLine[0].equals("ClientAuthToken")){
+                        strings.put(seperatedLine[0],"");    
+                        continue;
                     }
+                    System.out.println("Please provide an input for "+seperatedLine[0]);
+                    System.out.println("All inputs can be changed later in the config menu");
+                    String input = userInput.next();
+                    strings.put(seperatedLine[0],input);
+                    writeInput(seperatedLine[0],input);
+                }else if(seperatedLine[1].equals("http://:44344")){ //Add regex to verify IP input
+                    System.out.println("Please provide the IP of the Server\nPlease note that errors will need to be changed manually");
+                    String ip = "http://"+userInput.next()+":44344";
+                    strings.put(seperatedLine[0],ip);
+                    writeInput(seperatedLine[0], ip);
+                }else{
+                    strings.put(seperatedLine[0],seperatedLine[1]);
                 }
             }
         }
